48ad89eb59f1c08ea4432535169702a01e8e1b0c
Isaac Overacker <isaac@overacker.me>
2020-02-28 22:08:28 -0800

    Add a basic function to the lib and call it from the example app.

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 module.exports = {
+    watchFolders: [path.resolve(__dirname, '../dist')],
     transformer: {
         getTransformOptions: async () => ({
             transform: {
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const greeting = (): string => {
+    return 'Hello, world!';
+};
