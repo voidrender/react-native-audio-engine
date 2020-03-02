b94a45084c63182f028d03f48bd8429d6e5ce8e8
Isaac Overacker <isaac@overacker.me>
2020-03-01 22:37:40 -0800

    Example project is now TypeScript

--- example/App.tsx
+++ example/App.tsx
@@ -1,9 +1,9 @@
-import React, { Component, useState } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import { AudioEngine } from 'react-native-audio-engine';
 
 export const App = () => {
-    const [message, setMessage] = useState();
+    const [message, setMessage] = useState<string | undefined>();
 
     AudioEngine.sampleMethod('howdy!', 123, message => {
         setMessage(message);
@@ -11,7 +11,7 @@ export const App = () => {
 
     return (
         <View style={styles.container}>
-            <Text>{message || 'Waiting for NativeModule...'}</Text>
+            <Text>{message ?? 'Waiting for NativeModule...'}</Text>
         </View>
     );
 };
@@ -22,15 +22,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
     },
 });
--- ios/AudioEngine.m
+++ ios/AudioEngine.m
@@ -1,13 +1,54 @@
 #import "AudioEngine.h"
+#import <AVKit/AVKit.h>
+
+@interface AudioEngine()
+
+@property (nonatomic, strong) AVAudioEngine *engine;
+@property (nonatomic, strong) AVAudioMixerNode *mixer;
+
+@end
 
 @implementation AudioEngine
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.engine = [[AVAudioEngine alloc] init];
+        self.mixer = [[AVAudioMixerNode alloc] init];
+    }
+    return self;
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(pause)
+{
+    [self.engine pause];
+}
+
+RCT_EXPORT_METHOD(stop)
+{
+    [self.engine stop];
+}
+
+RCT_EXPORT_METHOD(addTrack:(NSString *)uri)
+{
+    AVAudioPlayerNode *node = [[AVAudioPlayerNode alloc] init];
+    [self.engine attachNode:node];
+    [self.engine connect:node to:self.mixer format:nil];
+}
 
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
 {
     // TODO: Implement some actually useful functionality
     callback(@[[NSString stringWithFormat: @"numberArgument!!: %@ stringArgument!!: %@", numberArgument, stringArgument]]);
 }
+
 
 @end
